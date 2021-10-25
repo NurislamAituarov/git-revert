add55f49a704db5d26b77bb09be4bc63a27e7e69
Nurislam <nurislam-1994@mail.ru>
2021-10-25 16:54:45 +0600

    sryjydt

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 (()=>{"use strict";window.addEventListener("DOMContentLoaded",(function(){console.log("Hello my friend"),document.querySelector("header").textContent="Я изменился",console.log("новый файл")}))})();
+=======
+(()=>{"use strict";window.addEventListener("DOMContentLoaded",(function(){document.querySelector("header").textContent="я изменился"}))})();
+>>>>>>> parent of 4205df2... sex
